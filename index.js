764eddabbc54d602af2d3956f9841774e99a0a8d
Vikas Pal <vikas.pal_2022@ltce.in>
2025-09-07 23:02:29 +0530

    Add existing RESTApi project files

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const path = require("path");
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/", (req,res) => {
+    res.send("server working well");
+});
+let posts = [
+    {
+        // username: "vikas",
+        title: "Post 1",
+        content: "This is the content of post 1"
+    },
+    {
+        // username: "john",
+        title: "Post 2",
+        content: "This is the content of post 2"
+    },
+    {
+        // username: "alice",
+        title: "Post 3",
+        content: "This is the content of post 3"
+    }
+];
+app.get("/posts",(req,res) => {
+    res.render("index.ejs", { posts });
+});
+app.get("/posts/new",(req,res) => {
+    res.render("new.ejs");
+})
+app.post("/posts", (req,res)  => {
+    let {title, content} = req.body;
+    posts.push({title,content});
+    res.send("data received");
+})
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
