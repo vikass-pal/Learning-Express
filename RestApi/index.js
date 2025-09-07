d2045bb30a3853f7b0c6a5009b72421e7f853163
Vikas Pal <vikas.pal_2022@ltce.in>
2025-09-08 00:26:13 +0530

    rest index

--- RestApi/index.js
+++ RestApi/index.js
@@ -10,7 +10,6 @@ app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
-
 app.get("/", (req,res) => {
     res.send("server working well");
 });
